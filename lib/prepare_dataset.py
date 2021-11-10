f0fac1866efa9a0095760b08e463d9fdeeb0623b
Honza <korinek.j@gmail.com>
2021-11-10 11:11:39 +0100

    Code cleaned

--- lib/prepare_dataset.py
+++ lib/prepare_dataset.py
@@ -9,7 +9,10 @@ import dateparser
 import pandas as pd
 
 
-def remove_partenthesis(row):
+def remove_parenthesis(row):
+    """
+    Function for parentheses removal.
+    """
     regex = re.compile(".*?\((.*?)\)")
     result = re.findall(regex, row)
 
@@ -27,6 +30,9 @@ def remove_partenthesis(row):
 
 
 def remove_MoM_QoQ(row):
+    """
+    Function for MoM and QoQ abbreviation removal.
+    """
     row = row.replace('MoM', '')
     row = row.replace('QoQ', '')
 
@@ -36,6 +42,9 @@ def remove_MoM_QoQ(row):
 
 
 def convert(el):
+    """
+    Function for units conversion into scientific numcer format.
+    """
     if isinstance(el, str):
         el = float(el.replace("M", "e+06") \
                    .replace(".0B", "e+09") \
@@ -48,6 +57,69 @@ def convert(el):
 
     return el
 
+def modify_sp500(raw):
+    """
+    Function cleans and modify S&P500 raw data into usable format for further
+    analysis.
+    :param raw: S&P500 raw data
+    :return: S&P500 cleaned DF
+    """
+    df_mod = raw.copy()
+    df_mod.replace(to_replace=',', value='', regex=True, inplace=True)
+    df_mod = df_mod[['Open', 'High', 'Low', 'Close', 'Volume']].astype('float64')
+    df_mod['Date'] = raw['Date'].apply(lambda x: dateparser.parse(x).date())  # Parse to datetime
+    df_mod.sort_values(by='Date', ascending=True, inplace=True)
+    df_mod.reset_index(inplace=True, drop=True)
+
+    return df_mod
+
+def modify_evens(raw):
+    """
+    Function cleans and modify calendar events raw data into usable format for further
+    analysis.
+    :param raw: Events raw data
+    :return: Events cleaned DF
+    """
+    df_mod = raw.copy()
+    df_mod['select'] = raw['Time'].str[:]
+    df_mod['select_2'] = df_mod['select'].fillna(method="ffill")
+    df_mod.dropna(axis=0, subset=['Event', 'Time'], inplace=True)
+    df_mod['Event'] = df_mod['Event'].apply(remove_parenthesis).apply(remove_MoM_QoQ)
+    to_drop = df_mod.loc[df_mod['select_2'] == 'All Day']
+    df_mod.drop(index=to_drop.index, inplace=True)
+    to_drop_2 = df_mod[df_mod['select_2'].str.len() < 5]
+    df_mod.drop(index=to_drop_2.index, inplace=True)
+    to_drop_3 = df_mod.loc[df_mod['select_2'] == 'Tentative']
+    df_mod.drop(index=to_drop_3.index, inplace=True)
+    df_mod = df_mod.drop(df_mod[df_mod['Actual'].isna()].index)
+    to_drop_4 = df_mod[df_mod['Actual'].str.len() < 2]
+    df_mod.drop(index=to_drop_4.index, inplace=True)
+    df_mod['Event'] = df_mod['Event'].str.split('(').str[0]
+    df_mod['Event'] = df_mod['Event'].str[:-1]
+    df_mod['Date'] = df_mod['select_2'].apply(lambda x: dateparser.parse(x).date())  # Parse to datetime
+    df_mod.drop(columns=['Cur.', 'Imp.', 'Forecast', 'Previous', 'select', 'select_2',], inplace=True)
+    df_mod = df_mod[['Date', 'Event', 'Actual']]
+    df_mod.reset_index(inplace=True, drop=True)
+
+    return df_mod
+
+def create_pivot(df):
+    """
+    Function pivot table from calendar events data
+    :param df: Calendar events data
+    :return: Pivoted Df
+    """
+    df_pivot = df.pivot_table(values='Actual', index=['Date'],
+                                             columns=['Event'], aggfunc=lambda x: x)
+    df_pivot = df_pivot.applymap(convert)
+    to_drop_5 = df_pivot[df_pivot['Building Permit'].str.len() < 2]
+    df_pivot.drop(index=to_drop_5.index, inplace=True)
+    to_drop_6 = df_pivot[df_pivot['New Home Sale'].str.len() < 2]
+    df_pivot.drop(index=to_drop_6.index, inplace=True)
+    df_pivot = df_pivot.astype('float64')
+
+    return df_pivot
+
 
 if __name__ == "__main__":
     # Runtime initiation
@@ -59,44 +131,13 @@ if __name__ == "__main__":
     raw_events= pd.read_excel('data/Events_scrape.xlsx')
 
     # S&P500 DF modifications
-    sp500 = raw_sp500.copy()
-    sp500.replace(to_replace=',', value='', regex=True, inplace=True)
-    sp500 = sp500[['Open', 'High', 'Low', 'Close', 'Volume']].astype('float64')
-    sp500['Date'] = raw_sp500['Date'].apply(lambda x: dateparser.parse(x).date())  # Parse to datetime
-    sp500.sort_values(by='Date', ascending=True, inplace=True)
-    sp500.reset_index(inplace=True, drop=True)
+    sp500 = modify_sp500(raw_sp500)
 
     # Events DF modifications
-    events = raw_events.copy()
-    events['select'] = raw_events['Time'].str[:]
-    events['select_2'] = events['select'].fillna(method="ffill")
-    events.dropna(axis=0, subset=['Event', 'Time'], inplace=True)
-    events['Event'] = events['Event'].apply(remove_partenthesis).apply(remove_MoM_QoQ)
-    to_drop = events.loc[events['select_2'] == 'All Day']
-    events.drop(index=to_drop.index, inplace=True)
-    to_drop_2 = events[events['select_2'].str.len() < 5]
-    events.drop(index=to_drop_2.index, inplace=True)
-    to_drop_3 = events.loc[events['select_2'] == 'Tentative']
-    events.drop(index=to_drop_3.index, inplace=True)
-    events = events.drop(events[events['Actual'].isna()].index)
-    to_drop_4 = events[events['Actual'].str.len() < 2]
-    events.drop(index=to_drop_4.index, inplace=True)
-    events['Event'] = events['Event'].str.split('(').str[0]
-    events['Event'] = events['Event'].str[:-1]
-    events['Date'] = events['select_2'].apply(lambda x: dateparser.parse(x).date())  # Parse to datetime
-    events.drop(columns=['Cur.', 'Imp.', 'Forecast', 'Previous', 'select', 'select_2',], inplace=True)
-    events = events[['Date', 'Event', 'Actual']]
-    events.reset_index(inplace=True, drop=True)
+    events = modify_evens(raw_events)
 
     # Events pivot table
-    events_pivot_actual = events.pivot_table(values='Actual', index=['Date'],
-                                             columns=['Event'], aggfunc=lambda x: x)
-    events_pivot_actual = events_pivot_actual.applymap(convert)
-    to_drop_5 = events_pivot_actual[events_pivot_actual['Building Permit'].str.len() < 2]
-    events_pivot_actual.drop(index=to_drop_5.index, inplace=True)
-    to_drop_6 = events_pivot_actual[events_pivot_actual['New Home Sale'].str.len() < 2]
-    events_pivot_actual.drop(index=to_drop_6.index, inplace=True)
-    events_pivot_actual = events_pivot_actual.astype('float64')
+    events_pivot_actual = create_pivot(events)
 
     # Merge datasets
     sp500_calendar = sp500.merge(events_pivot_actual, how='outer', left_on='Date',
@@ -111,5 +152,3 @@ if __name__ == "__main__":
     m, s = divmod(time.time()-run_start,60)
     h, m = divmod(m, 60)
     print('Dataset processing finished in:', '%d:%02d:%02d'%(h, m, s))
-
-    a=1
